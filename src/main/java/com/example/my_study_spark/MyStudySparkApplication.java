5319cac15c6682f24aee54966ac5dae6a80eb4aa
liushouhang <liushouhang@hhkj17.wecom.work>
2024-05-20 11:15:54 +0800

    20240520

---
+++ src/main/java/com/example/my_study_spark/MyStudySparkApplication.java
@@ -0,0 +1,14 @@
+package com.example.my_study_spark;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudySparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStudySparkApplication.class, args);
+    }
+
+}
